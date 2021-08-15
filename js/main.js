fcab8ee9b4d2748f2994da8f9d3dc3976b9d34f9
thanhluanbk <nhocluan97@gmail.com>
2021-08-15 09:24:54 +0700

    Upload

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ function firstQuestion(){
 }
 // move random button póition
 function moveButton() {
-    var audio = new Audio('sound/Swish1.mp3');
+    var audio = new Audio('sound/1.mp3');
     audio.play();
     var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9 ;
     var y = Math.random() * ($(window).height() - $('#no').height()) * 0.9;
